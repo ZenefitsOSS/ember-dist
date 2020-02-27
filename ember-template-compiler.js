10fea2ae05eae02dc92184310c27435ac7fd4417
kumarappan-arumugam <kumarappan@zenefits.com>
2020-02-26 20:14:15 -0800

    log LOG_DEPRECATIONS ENV to test

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -223,7 +223,9 @@ enifed("ember-debug", ["exports", "ember-metal/core", "ember-metal/error", "embe
     @public
   */
   _emberMetalCore["default"].deprecate = function (message, test, options) {
-    if (!_emberMetalCore["default"].LOG_DEPRECATIONS) {
+
+    _emberMetalLogger["default"].warn("LOG_DEPRECATIONS:" + _emberMetalCore["default"].LOG_DEPRECATIONS);
+    if (!!_emberMetalCore["default"].LOG_DEPRECATIONS) {
       return;
     }
     if (_emberMetalCore["default"].ENV.RAISE_ON_DEPRECATION) {
